456d4ab222c31a5695fd73fd848b6f924ca77c5b
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-26 01:03:48 +0800

    feat: tambah element basic

--- src/app/dokter/[id]/janji/page.jsx
+++ src/app/dokter/[id]/janji/page.jsx
@@ -1,3 +1,21 @@
 export default function Page() {
-    return <main className="w-full">Buat Janji</main>
+    return (
+        <main className="w-full">
+            <h1>Buat Janji</h1>
+            <aside>
+                <div>
+                    <h2>Info Dokter</h2>
+                    
+            <p>Spesialis</p>
+                </div>
+                <div>
+                    <h2>Info Rumah Sakit</h2>
+                </div>
+            </aside>
+            <div>
+                <button>Tambah Keluarga</button>
+                <form></form>
+            </div>
+        </main>
+    )
 }
