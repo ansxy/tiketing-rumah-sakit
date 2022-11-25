7e21d5f9ae515fabcb0d076363241a0407acb539
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-25 22:07:07 +0800

    feat: adding components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [],
+    theme: {
+        extend: {
+        colors: {
+              
+            },
+        },
+    },
+    plugins: [],
 }
