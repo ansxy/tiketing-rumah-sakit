7146f50c9644205eacd7191206e8ff3265524453
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-25 20:13:20 +0800

    refactor: membersihkan boilerplate

--- pages/api/rumahsakit/klinik/praktek/jam/[jam].jsx
+++ pages/api/rumahsakit/klinik/praktek/jam/[jam].jsx
@@ -1,31 +1,29 @@
-import { createRouter } from "next-connect";
-import prisma from "../../../../../../lib/prisma";
-
-
+import { createRouter } from "next-connect"
+import prisma from "../../../../../../lib/prisma"
 
 const apiRoute = createRouter({
-    onNoMatch(req,res) {
+    onNoMatch(req, res) {
         res.status(405).json({
-            error : `Method '${req.method}' Not Allowed`
+            error: `Method '${req.method}' Not Allowed`,
         })
     },
-}).post(async (req,res) => {
-    const {start_time,end_time} = req.body
-    const {jam} = req.query
+}).post(async (req, res) => {
+    const { start_time, end_time } = req.body
+    const { jam } = req.query
     try {
         const createJamPraktek = await prisma.jam_praktek.create({
-            data : {
-                dokter_hari_praktekid : jam,
-                start_time : start_time,
-                end_time : end_time,
-            }
+            data: {
+                dokter_hari_praktekid: jam,
+                start_time: start_time,
+                end_time: end_time,
+            },
         })
         return res.status(200).json({
-            status : "success",
-            data : createJamPraktek
+            status: "success",
+            data: createJamPraktek,
         })
     } catch (error) {
-        return res.status(404).json({ status : error.message})
+        return res.status(404).json({ status: error.message })
     }
 })
 // .get(async(req,res) => {
@@ -44,9 +42,9 @@ const apiRoute = createRouter({
 //         res.status(404).json({ status : error.message})
 //     }
 // })
-export default apiRoute.handler();
+export default apiRoute.handler()
 export const config = {
-  api: {
-    bodyParser: true, // Disallow body parsing, consume as stream
-  },
-};
+    api: {
+        bodyParser: true, // Disallow body parsing, consume as stream
+    },
+}
