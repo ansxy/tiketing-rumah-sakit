4f5ac241a8dfa2a4ec70361447eb3d924c1a6827
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-25 23:35:43 +0800

    feat: tambah home, navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
+    content: [
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {
-        colors: {
-              
+            colors: {
+                "healtick-darkgreen": "#285430",
+                "healtick-green": "#5F8D4E",
+                "healtick-lightgreen": "#A4BE7B",
+                "healtick-cream": "#E5D9B6",
             },
         },
     },
