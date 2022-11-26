6ea33fcf2d99c3cbb779645680ca18689d409cdf
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-26 23:01:01 +0800

    fix: perbaiki keadaan

--- src/lib/fetch-dokter.jsx
+++ src/lib/fetch-dokter.jsx
@@ -1,5 +1,5 @@
 export async function FetchDokter({ id }) {
-    const res = await fetch(`http://localhost:3000/api/user/dokter/${id}`)
+    const res = await fetch(`/api/user/dokter/${id}`)
 
     if (!res.ok) {
         console.log("error getting data")
--- src/lib/fetch-dokters.jsx
+++ src/lib/fetch-dokters.jsx
@@ -1,5 +1,5 @@
 export async function FetchDokters() {
-    const res = await fetch("http://localhost:3000/api/user/dokter/")
+    const res = await fetch("/api/user/dokter/")
 
     if (!res.ok) {
         console.log("error getting data")
