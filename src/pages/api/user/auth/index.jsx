3bed00b174fe88960d50314ceaa98f679414d3d6
ansxy <53747702+ansxy@users.noreply.github.com>
2022-11-26 17:31:51 +0800

    Login with google

--- src/pages/api/user/auth/index.jsx
+++ src/pages/api/user/auth/index.jsx
@@ -18,6 +18,7 @@ const apiRoute = createRouter({
     }
 }).get(async (req,res) => {
     const {email , password} = req.body
+    console.log(req.body)
     try{
         const verifiyLogin = await prisma.pasien.findFirst({
             where : {
